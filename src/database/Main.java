e8a94c2d9d3bdaae9b22a9910c09e4890248e3f1
Rogine Ceniza <rogineceniza0770@outlook.com>
2025-12-13 15:26:42 +0800

    Initial commit: database setup

---
+++ src/database/Main.java
@@ -0,0 +1,8 @@
+package database;
+
+public class Main {
+    public static void main(String[] args) {
+        new DatabaseManager();
+        System.out.println("Program finished successfully!");
+    }
+}
